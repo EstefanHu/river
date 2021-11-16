7aac8bb6d3f5b01d90cb15bf376107a4508cedb5
EstefanHu <estefan404@gmail.com>
2021-11-15 19:02:51 -0800

    updated nav toggler

---
+++ wrappers/writeWrapper/WriteWrapper.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from './WriteWrapper.module.scss'
+
+const WriteWrapper = () => {
+
+    return (
+        <div className={styles.WriteWrapper}>
+
+        </div>
+    )
+}
+
+export default WriteWrapper
